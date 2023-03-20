2dd3a6b772a9cf2789871f25e3629323fe599deb
Anton Charny <anton.charny@perimeter81.com>
2023-03-20 15:43:19 +0200

    Done

--- SwiftMarathon7/ViewController.swift
+++ SwiftMarathon7/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private lazy var scrollView = UIScrollView(frame: self.view.bounds)
+    private lazy var imageView = UIImageView(image: UIImage(named: "header-image.jpg"))
+    
+    private let imageOriginalHeight: CGFloat = 270
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        scrollView.contentSize = .init(width: view.bounds.width,
+                                       height: view.bounds.height + imageOriginalHeight)
+        scrollView.contentInsetAdjustmentBehavior = .never
+        scrollView.delegate = self
+        scrollView.backgroundColor = .purple
+        view.addSubview(scrollView)
+        
+        imageView.contentMode = .scaleAspectFill
+        imageView.frame = .init(origin: .zero, size: .init(width: self.view.bounds.size.width,
+                                                           height: imageOriginalHeight))
+        imageView.clipsToBounds = true
+        scrollView.addSubview(imageView)
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let height = imageOriginalHeight - scrollView.contentOffset.y
+        imageView.frame = .init(x: 0, y: scrollView.contentOffset.y, width: self.view.bounds.width, height: height)
+        if height > 0 {
+            scrollView.showsVerticalScrollIndicator = true
+            let indicatorInset = max(height, imageOriginalHeight) - view.safeAreaInsets.top
+            scrollView.verticalScrollIndicatorInsets = .init(top: indicatorInset,
+                                                             left: 0,
+                                                             bottom: 0,
+                                                             right: 0)
+        } else {
+            scrollView.showsVerticalScrollIndicator = false
+        }
+        
+        print(height)
+        print(scrollView.contentOffset.y)
+    }
+}
